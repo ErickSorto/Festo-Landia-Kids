f13632f575af7aae82025c31aed2123756cb6533
Erick <sortoerick33@gmail.com>
2024-07-07 01:21:03 -0400

    Add UIKitView for IOS Sun Logo

--- composeApp/src/iosMain/kotlin/PlatformUtils.kt
+++ composeApp/src/iosMain/kotlin/PlatformUtils.kt
@@ -1,46 +1,25 @@
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.interop.UIKitView
-import kotlinx.cinterop.ExperimentalForeignApi
-import platform.UIKit.*
+import androidx.compose.ui.layout.ContentScale
+import festolandiakids.composeapp.generated.resources.FestoLandiaKidsSunLogo
+import festolandiakids.composeapp.generated.resources.FestoSunLogo
+import festolandiakids.composeapp.generated.resources.Res
+import org.jetbrains.compose.resources.painterResource
+
 @Composable
 actual fun setTranslucentBars(darkTheme: Boolean) {
     // no-op
 }
 
 
-@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun SunAnimationView(modifier: Modifier) {
-    UIKitView(
+    Image(
+        painter = painterResource(
+            Res.drawable.FestoSunLogo),
+        contentDescription = null,
         modifier = modifier,
-        factory = {
-            UIView().apply {
-                backgroundColor = UIColor.clearColor
-                opaque = false
-                setClipsToBounds(true)
-            }
-        },
-        background = Color.Transparent,
-        update = {
-            val imageView = UIImageView().apply {
-                image = UIImage.imageNamed("FestoLandiaKidsSunLogo") // Replace with your actual PNG resource name
-                translatesAutoresizingMaskIntoConstraints = false
-                contentMode = UIViewContentModeScaleAspectFit
-                backgroundColor = UIColor.clearColor
-                opaque = false
-            }
-            it.addSubview(imageView)
-
-            NSLayoutConstraint.activateConstraints(
-                listOf(
-                    imageView.widthAnchor.constraintEqualToAnchor(it.widthAnchor),
-                    imageView.heightAnchor.constraintEqualToAnchor(it.heightAnchor),
-                    imageView.centerXAnchor.constraintEqualToAnchor(it.centerXAnchor),
-                    imageView.centerYAnchor.constraintEqualToAnchor(it.centerYAnchor)
-                )
-            )
-        }
+        contentScale = ContentScale.Fit
     )
 }
